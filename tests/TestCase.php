5415d8a4161ca459d602cfca018eaeadc6247121
Morteza Masumi <morteza.masumi@gmail.com>
2025-10-25 09:32:26 +0330

    minor in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,10 +3,12 @@
 namespace Mortezamasumi\FbActivity\Tests;
 
 use Filament\Facades\Filament;
+use Filament\Http\Middleware\Authenticate;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
 use Mortezamasumi\FbActivity\FbActivityPlugin;
 use Mortezamasumi\FbActivity\FbActivityServiceProvider;
@@ -35,6 +37,53 @@ class TestCase extends TestbenchTestCase
             $table->timestamps();
         });
 
+        Schema::create('exports', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('completed_at')->nullable();
+            $table->string('file_disk');
+            $table->string('file_name')->nullable();
+            $table->string('exporter');
+            $table->unsignedInteger('processed_rows')->default(0);
+            $table->unsignedInteger('total_rows');
+            $table->unsignedInteger('successful_rows')->default(0);
+            /** @disregard */
+            $table->foreignIdFor(Auth::getProvider()->getModel())->cascadeOnDelete();
+            $table->timestamps();
+        });
+
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+
+        Schema::create('job_batches', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('name');
+            $table->integer('total_jobs');
+            $table->integer('pending_jobs');
+            $table->integer('failed_jobs');
+            $table->longText('failed_job_ids');
+            $table->mediumText('options')->nullable();
+            $table->integer('cancelled_at')->nullable();
+            $table->integer('created_at');
+            $table->integer('finished_at')->nullable();
+        });
+
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
+        });
+
         Filament::registerPanel(
             Panel::make()
                 ->id('admin')
@@ -47,6 +96,9 @@ class TestCase extends TestbenchTestCase
                 ->plugins([
                     FbEssentialsPlugin::make(),
                     FbActivityPlugin::make(),
+                ])
+                ->authMiddleware([
+                    Authenticate::class,
                 ])
         );
     }
--- tests/Tests/ActivityResourceTest.php
+++ tests/Tests/ActivityResourceTest.php
@@ -1,68 +1,151 @@
 <?php
 
+use Filament\Actions\Exports\Models\Export;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Mortezamasumi\FbActivity\Facades\FbActivity;
-use Mortezamasumi\FbActivity\Resources\FbActivityResource;
+use Mortezamasumi\FbActivity\Resources\Exports\ActivityExporter;
 use Mortezamasumi\FbActivity\Resources\Pages\ListActivity;
 use Mortezamasumi\FbActivity\Resources\Pages\ViewActivity;
+use Mortezamasumi\FbActivity\Resources\FbActivityResource;
 use Mortezamasumi\FbActivity\Tests\Services\Podcast;
 use Mortezamasumi\FbActivity\Tests\Services\User;
 use Mortezamasumi\FbEssentials\Facades\FbPersian;
+use Spatie\Activitylog\Models\Activity;
 
-beforeEach(function () {
-    Gate::before(fn () => true);
+describe('as guest/un-authorized user', function () {
+    it('guests cannot access the resource', function () {
+        Podcast::factory()->create();
 
-    $this->actingAs(User::factory()->create());
+        $this
+            ->get(FbActivityResource::getUrl('index'))
+            ->assertRedirect(config('filament.auth.pages.login'));
+
+        $this
+            ->get(FbActivityResource::getUrl('view', ['record' => Activity::first()]))
+            ->assertRedirect(config('filament.auth.pages.login'));
+    });
+
+    it('un-authorized users cannot access the resource', function () {
+        $this->actingAs(User::factory()->create());
+
+        Podcast::factory()->create();
+
+        $this
+            ->get(FbActivityResource::getUrl('index'))
+            ->assertForbidden();
+
+        $this
+            ->get(FbActivityResource::getUrl('view', ['record' => Activity::first()]))
+            ->assertForbidden();
+    });
 });
 
-it('can render index page', function () {
-    $this
-        ->get(FbActivityResource::getUrl('index'))
-        ->assertSuccessful();
-});
+describe('as authorized user', function () {
+    beforeEach(function () {
+        Gate::before(fn () => true);
 
-it('can show empty table', function () {
-    $this
-        ->livewire(ListActivity::class)
-        ->assertCanSeeTableRecords([])
-        ->assertCountTableRecords(0);
-});
+        $this->actingAs(User::factory()->create());
+    });
 
-it('can show data in table', function () {
-    $podcast = Podcast::factory()->create();
+    it('can render index page', function () {
+        $this
+            ->get(FbActivityResource::getUrl('index'))
+            ->assertSuccessful();
+    });
 
-    $count = 6;
+    it('can show empty table', function () {
+        $this
+            ->livewire(ListActivity::class)
+            ->assertCanSeeTableRecords([])
+            ->assertCountTableRecords(0);
+    });
 
-    for ($i = 1; $i < $count; $i++) {
-        $podcast->update(['text' => fake()->sentence()]);
-    }
+    it('can show data in table', function () {
+        $podcast = Podcast::factory()->create();
 
-    $this
-        ->livewire(ListActivity::class)
-        ->assertCanSeeTableRecords($podcast->activities)
-        ->assertCountTableRecords($count);
-});
+        $count = 6;
 
-it('can view by url', function () {
-    $activity = Podcast::factory()->create()->activities->first();
+        for ($i = 1; $i < $count; $i++) {
+            $podcast->update(['text' => fake()->sentence()]);
+        }
 
-    $this
-        ->get(FbActivityResource::getUrl('view', [
-            'record' => $activity->getRouteKey(),
-        ]))
-        ->assertSuccessful();
-});
+        $this
+            ->livewire(ListActivity::class)
+            ->assertCanSeeTableRecords($podcast->activities)
+            ->assertCountTableRecords($count);
+    });
 
-it('can retrieve data', function () {
-    $activity = Podcast::factory()->create()->activities->first();
+    it('can view by url', function () {
+        $activity = Podcast::factory()->create()->activities->first();
 
-    $this
-        ->livewire(ViewActivity::class, [
-            'record' => $activity->getRouteKey(),
-        ])
-        ->assertSeeText(FbActivity::getSubject($activity, $activity['subject_type']))
-        ->assertSeeText($activity->description)
-        ->assertSeeText(ucwords($activity->log_name))
-        ->assertSeeText(ucwords($activity->event))
-        ->assertSeeText(FbPersian::jDateTime(null, $activity->created_at));
+        $this
+            ->get(FbActivityResource::getUrl('view', [
+                'record' => $activity->getRouteKey(),
+            ]))
+            ->assertSuccessful();
+    });
+
+    it('can view data', function () {
+        $activity = Podcast::factory()->create()->activities->first();
+
+        $this
+            ->livewire(ViewActivity::class, [
+                'record' => $activity->getRouteKey(),
+            ])
+            ->assertSeeText(FbActivity::getSubject($activity, $activity['subject_type']))
+            ->assertSeeText($activity->description)
+            ->assertSeeText(ucwords($activity->log_name))
+            ->assertSeeText(ucwords($activity->event))
+            ->assertSeeText(FbPersian::jDateTime(null, $activity->created_at));
+    });
+
+    it('can export activities and verify downloaded csv file', function () {
+        Activity::truncate();
+
+        $count = 6;
+
+        Podcast::factory($count)->create();
+
+        $this
+            ->actingAs(User::factory()->create())
+            ->livewire(ListActivity::class)
+            ->callAction('export-activities')
+            ->assertNotified();
+
+        $export = Export::latest()->first();
+
+        expect($export)
+            ->not
+            ->toBeNull()
+            ->processed_rows
+            ->toBe($count)
+            ->successful_rows
+            ->toBe($count)
+            ->completed_at
+            ->not
+            ->toBeNull();
+
+        $this
+            ->get(route(
+                'filament.exports.download',
+                ['export' => $export, 'format' => 'csv'],
+                absolute: false
+            ))
+            ->assertDownload()
+            ->tap(function ($response) {
+                $content = $response->streamedContent();
+
+                foreach (collect(ActivityExporter::getColumns())->map(fn ($column) => $column->getLabel()) as $label) {
+                    expect($content)
+                        ->toContain($label);
+                };
+
+                foreach (Podcast::all() as $podcast) {
+                    expect($content)
+                        ->toContain($podcast->id)
+                        ->toContain($podcast->text);
+                }
+            });
+    });
 });
