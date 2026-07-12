9e6f221c82a293a64846877f0b2a6785b30afd48
Morteza Masumi <morteza.masumi@gmail.com>
2026-07-12 10:22:44 +0330

    fix: minor

--- src/Resources/Pages/ListActivity.php
+++ src/Resources/Pages/ListActivity.php
@@ -20,7 +20,7 @@ class ListActivity extends ListRecords
                 ->modalSubmitActionLabel(__('fb-activity::fb-activity.export.action'))
                 ->exporter(config('fb-activity.export.exporter'))
                 ->maxRows(config('fb-activity.export.max_export_rows'))
-                ->visible(Auth::user()->can('Export:Activity')),
+                ->visible(fn() => (bool) Auth::user()?->can('Export:Activity')),
         ];
     }
 }
